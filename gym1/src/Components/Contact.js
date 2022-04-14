dd41752a68165eb38de68329180026e7f26082f1
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-04-14 18:14:40 +0530

    gym

---
+++ gym1/src/Components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div id='contact'>
+        <h1>CONTACT US</h1>
+        <form>
+            <input type='text' placeholder='Full Name' required/>
+            <input type='email' placeholder='Email' required/>
+            <textarea placeholder="write here ........." name='message'></textarea>
+            <input type='submit' value='send'/>
+        </form>
+        </div>
+  )
+}
---
+++ gym1/src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div id='main'>
+        <div className='header-heading'>
+          <div className='hy'><h2>steps UP Yours</h2>
+            <h1><span>FITNNESS FUNDA</span> WITH US</h1>
+            <p className='details'>Build Your Body And Fitness with ME
+            </p>
+            <div className='header-btns'>
+                <a href='#' className='header-btn'>JOIN us</a></div>
+            
+            </div>
+        </div>
+
+    </div>
+  )
+}
